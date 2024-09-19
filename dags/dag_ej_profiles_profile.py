3e7690cb21f9fe72da2e429d4aa374124581ba88
Eric Silveira <es9694649@gmail.com>
2024-09-19 11:51:38 -0300

    feat(ingestion): criando nova dag de ingestao e transformacao

--- dags/dag_ej_profiles_profile.py
+++ dags/dag_ej_profiles_profile.py
@@ -4,11 +4,12 @@ from datetime import datetime, timedelta
 from scripts.database_etl.base import SourceTables
 from scripts.database_etl import PostgresETL
 from scripts.database_etl.utils import setup_logging
+from dags.scripts.transformations import transformation_ej_profiles_profile
 
 default_args = {
-    "owner": "Giovani Giampauli / Eric Silveira",
+    "owner": "Eric Silveira",
     "depends_on_past": False,
-    "email": ["giovanni.acg@gmail.com"],
+    "email": ["silveirames@hotmail.com"],
     "email_on_failure": True,
     "email_on_retry": False,
     "retries": 0,
@@ -17,9 +18,9 @@ default_args = {
 
 
 @dag(
-    dag_id="clone_rasa_events",
+    dag_id="clone_and_transform_ej_profiles_profile",
     default_args=default_args,
-    description="DAG para executar operações ETL da tabela de eventos do Rasa",
+    description="DAG para executar operações ETL da tabela ej_profiles_profile",
     schedule_interval="@daily",
     start_date=datetime(2023, 10, 1),
     catchup=False,
@@ -53,6 +54,7 @@ def clone_rasa_events():
             chunk_size=50000,
             max_threads=10,
             multithreading=True,
+            transform_func=transformation_ej_profiles_profile,
         )
 
         etl.clone_tables_incremental(source_tables, target_table, key_column)
